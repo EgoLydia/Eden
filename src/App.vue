52c679061ad42300c3a4eacbe65f45adbb91163e
Lydia <egolydia97@gmail.com>
2022-02-06 22:57:23 +0100

    Feature: Add media queries for responsiveness

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,65 @@
   color: #2c3e50;
 }
 
-.container{
+* {
+   --full-height: 100%;
+}
+
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+@media only screen and (max-width: 640px) {
+  .container {
+    padding-left: 1rem;
+    padding-right: 1rem;
+    margin-top: -2.5rem;
+  }
+
+  .card-list {
+    column-gap: 3rem;
+    column-fill: initial;
+    column-count: 1;
+  }
+}
+
+@media (min-width: 640px) {
+  .container {
     padding-left: 5rem;
     padding-right: 5rem;
     margin-top: -2.5rem;
   }
+
+  .card-list {
+    column-gap: 3rem;
+    column-fill: initial;
+    column-count: 1;
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    padding-left: 5rem;
+    padding-right: 5rem;
+    margin-top: -2.5rem;
+  }
+
+  .card-list {
+    column-gap: 3rem;
+    column-fill: initial;
+    column-count: 3;
+  }
+}
+
+@media (min-width: 1024px) {
+  .container {
+    padding-left: 14rem;
+    padding-right: 14rem;
+    margin-top: -2.5rem;
+  }
+}
+
 </style>
