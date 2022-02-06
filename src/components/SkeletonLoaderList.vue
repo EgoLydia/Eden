19179222640de7d166f6d3725768c56ba15c2efb
Lydia <egolydia97@gmail.com>
2022-02-06 22:49:16 +0100

    Feature: Add skeleton loader list component

---
+++ src/components/SkeletonLoaderList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="loader-list">
+        <skeleton-loader></skeleton-loader>
+    </div>
+</template>
+
+<script>
+import SkeletonLoader from '../components/SkeletonLoader.vue'
+export default {
+    name: "SkeletonLoaderList",
+  components: {
+      "skeleton-loader": SkeletonLoader,
+       },
+};
+</script>
+
+<style scoped lang="scss">
+.loader {
+  margin-bottom: 2rem;
+  vertical-align: top;
+  display: inline-flex;
+  width: var(--full-height);
+}
+
+</style>
