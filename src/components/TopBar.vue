3c4a151f745b333c2cb53e35e95d7415f6555701
Lydia <egolydia97@gmail.com>
2022-02-06 22:58:54 +0100

    Refactor: Edit style

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,25 +1,25 @@
 <template>
-<div>
-<div class="top-bar-bg">  
-    <slot></slot>
-</div>
-</div>
+  <div>
+    <div class="top-bar-bg">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TopBar",
-    components:{},
-    data() {
-        return{};
-    }
-    
-}
+  name: "TopBar",
+  components: {},
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.top-bar-bg{
-  padding: 5rem;
+.top-bar-bg {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
   background-color: #dde2e9;
 }
 </style>
