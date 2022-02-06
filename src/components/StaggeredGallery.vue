14fbca76e79e8a2a403a8ebb1b6dafd9a04a89d8
Lydia <egolydia97@gmail.com>
2022-02-06 01:37:28 +0100

    Feature: Add gallery component

---
+++ src/components/StaggeredGallery.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card-list">
+    <card
+      class="card-item"
+      v-for="(image, index) in images"
+      :key="index"
+      :image="image"
+      :breed="getBreed(image)"
+    ></card>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+  name: "staggeredGallery",
+  props: ["images"],
+  components: {
+    card: Card,
+  },
+  methods: {
+    getBreed(url) {
+        
+      let path = url.replace("https://images.dog.ceo/breeds/", "");
+      const index = path.indexOf("/");
+      return path.slice(0,index);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card-item {
+  margin-bottom: 2rem;
+  vertical-align: top;
+}
+
+.card-list {
+  column-gap: 3rem;
+  column-fill: initial;
+  column-count: 3;
+}
+</style>
