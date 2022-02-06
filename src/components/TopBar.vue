a691f9e9d7efc024f11ee8ce3636f39e65a1e401
Lydia <egolydia97@gmail.com>
2022-02-06 01:36:49 +0100

    Feature: Add  top bar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+<div class="top-bar-bg">  
+    <slot></slot>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "TopBar",
+    components:{},
+    data() {
+        return{};
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.top-bar-bg{
+  padding: 5rem;
+  background-color: #dde2e9;
+}
+</style>
