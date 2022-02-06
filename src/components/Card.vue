f134d8b2f63bccc3751b7117090a8d9aba5ecd58
Lydia <egolydia97@gmail.com>
2022-02-06 21:13:02 +0100

    Feature: import skeleton loader component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,36 +1,47 @@
 <template>
-    <div class="card"> 
-        <img class="card-img" :src="image" alt="Thumbnail for dogs">
-        <div class="card-overlay">
-            <div class="card-footer">
-                <p class="card-title">{{ breed }}</p>
-            </div>
+  <div>
+    <skeleton-loader :isloading="isloading"></skeleton-loader>
+    <div v-show="!isloading" class="card">
+      <img
+        @load="onloaded"
+        class="card-img"
+        :src="image"
+        alt="Thumbnail for dogs"
+      />
+      <div class="card-overlay">
+        <div class="card-footer">
+          <p class="card-title">{{ breed }}</p>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import SkeletonLoader from "./SkeletonLoader.vue";
+
 export default {
-    name: "Card",
-    props: ["image", "breed"],
+  name: "Card",
+  props: ["image", "breed"],
 
-    data() {
-        return{
-            isloading: true,
-        };
+  data() {
+    return {
+      isloading: true,
+    };
+  },
+  components: {
+    "skeleton-loader": SkeletonLoader,
+  },
+  methods: {
+    onloaded() {
+      this.isloading = false;
     },
-    components: {},
-    methods: {
-        onloaded(){
-            this.isloading = true;
-        }
-    }
-}
-
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .card {
+.card {
   border-radius: 0.6rem;
   overflow: hidden;
   position: relative;
@@ -58,7 +69,11 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 1));
+  background-image: linear-gradient(
+    rgba(0, 0, 0, 0),
+    rgba(0, 0, 0, 0.2),
+    rgba(0, 0, 0, 1)
+  );
   width: 100%;
   height: 100%;
   display: flex;
@@ -99,5 +114,4 @@ export default {
   padding: 0.625rem;
   padding-left: 1.5rem;
 }
-
 </style>
